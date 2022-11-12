9bae8f22808c230147f03238c0c6ca0f781fabb9
Eduardo Barros <eduardo.barros@executiva.com.br>
2022-11-12 01:46:14 -0300

    Update Requests and Api Routes

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Api\V1\BalanceController;
+use App\Http\Controllers\Api\V1\EventController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +16,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::resource('balance', BalanceController::class)->only([
+    'index'
+]);
+
+Route::resource('event', EventController::class)->only([
+    'store'
+]);
