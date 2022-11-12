f357b0843909401871a476494c7a761bf0456da6
Eduardo Barros <eduardo.barros@executiva.com.br>
2022-11-12 02:42:04 -0300

    EventRequest Update Validator

--- app/Http/Controllers/Api/V1/EventController.php
+++ app/Http/Controllers/Api/V1/EventController.php
@@ -3,12 +3,13 @@
 namespace App\Http\Controllers\Api\V1;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\EventRequest;
 use Illuminate\Http\Request;
 
 class EventController extends Controller
 {
-    public function store()
+    public function store(EventRequest $eventRequest)
     {
-
+        return $eventRequest;
     }
 }
