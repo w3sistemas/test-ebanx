e1cf25bb0ab8a479836b5b5cce18e3df4935b41f
Eduardo Barros <eduardo.barros@executiva.com.br>
2022-11-12 03:13:37 -0300

    Update Code

--- app/Http/Controllers/Api/V1/BalanceController.php
+++ app/Http/Controllers/Api/V1/BalanceController.php
@@ -4,11 +4,12 @@ namespace App\Http\Controllers\Api\V1;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\BalanceRequest;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
 class BalanceController extends Controller
 {
-    public function index(BalanceRequest $balanceRequest)
+    public function index(BalanceRequest $balanceRequest): JsonResponse
     {
         return response()->json(0, 404);
     }
--- app/Http/Controllers/Api/V1/EventController.php
+++ app/Http/Controllers/Api/V1/EventController.php
@@ -4,12 +4,20 @@ namespace App\Http\Controllers\Api\V1;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\EventRequest;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
 class EventController extends Controller
 {
-    public function store(EventRequest $eventRequest)
+    public function store(EventRequest $eventRequest): JsonResponse
     {
-        return $eventRequest;
+        return response()->json([
+            [
+                "destination" => [
+                    "id" => "100",
+                    "balance" => 10
+                ]
+            ]
+        ], 201);
     }
 }
