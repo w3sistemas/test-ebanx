196fadb56c5018633191f7ac5b1ed10716a45628
Eduardo Barros <eduardo.barros@executiva.com.br>
2022-11-12 02:11:09 -0300

    BalanceRequest Update Validator

---
+++ app/Http/Controllers/Api/V1/BalanceController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\BalanceRequest;
+use Illuminate\Http\Request;
+
+class BalanceController extends Controller
+{
+    public function index(BalanceRequest $balanceRequest)
+    {
+        return $balanceRequest;
+    }
+}
---
+++ app/Http/Requests/BalanceRequest.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+
+class BalanceRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules(): array
+    {
+        return [
+            'account_id' => 'required'
+        ];
+    }
+
+    public function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(response()->json([
+            'success'   => false,
+            'message'   => 'Validation errors',
+            'data'      => $validator->errors()
+        ]));
+    }
+
+    public function messages(): array
+    {
+        return [
+            'account_id.required' => 'Account id is required'
+        ];
+
+    }
+}
