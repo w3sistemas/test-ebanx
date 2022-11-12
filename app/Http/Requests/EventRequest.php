8bc2049114f344d59f22c70ae07e7fc2d8f416ee
Eduardo Barros <eduardo.barros@executiva.com.br>
2022-11-12 02:22:27 -0300

    EventRequest Update Validator

--- app/Http/Requests/EventRequest.php
+++ app/Http/Requests/EventRequest.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Requests;
 
+use Illuminate\Contracts\Validation\Validator;
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
 
 class EventRequest extends FormRequest
 {
@@ -28,5 +30,24 @@ class EventRequest extends FormRequest
             'destination' => 'required',
             'amount' => 'required'
         ];
+    }
+
+    public function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(response()->json([
+            'success'   => false,
+            'message'   => 'Validation errors',
+            'data'      => $validator->errors()
+        ]));
+    }
+
+    public function messages(): array
+    {
+        return [
+            'type.required' => 'Type is required',
+            'destination.required' => 'Destination is required',
+            'amount.required' => 'Amount is required'
+        ];
+
     }
 }
